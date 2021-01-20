f3e5ac2c4a3cd5f57e69e47119bc9717b9370a6b
Jcwells18 <jwells1@fhlbc.com>
2021-01-20 13:54:41 -0600

    test calc for candidate votes and %

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,7 +5,7 @@ import csv
 #create arrays
 votes = []
 candidates = []
-candidate = []
+
 
 #find election data csv
 electiondata_csv = os.path.join("Resources","election_data.csv")
@@ -17,17 +17,38 @@ with open(electiondata_csv,'r',newline='') as csv_file:
     #identify header
     csv_header = next(csv_file)
 
-    #set number of votes to zero
+    #set variables
     num_votes = 0
+    khanvote = 0
+    correyvote = 0
+    livote = 0
+    otooleyvote = 0
+    khanpercent = 0.00
+    correypercent = 0.00
+    lipercent = 0.00
+    otooleypercent = 0.00
 
-    #for loop to count number of votes
+    #for loop to count number of votes and find candidate
     for row in csv_reader:
         votes.append(row[0])
-        candidate.append(row[2])
 
+        #calculate total number of votes for each candidate
+        if (row[2] =="Khan"):
+            khanvote = khanvote + 1
+        elif (row[2] =="Correy"):
+            correyvote = correyvote + 1
+        elif (row[2] == "Li"):
+            livote = livote + 1
+        elif(row[2] == "O'Tooley"):
+            otooleyvote = otooleyvote + 1
+        #calculate total votes
+        total_votes = int(len(votes))
 
-#calculate total votes
-total_votes = int(len(votes))
+        #calculcate percent
+        khanpercent = round(khanvote/total_votes*100)
+        correypercent = round(correyvote/total_votes*100)
+        lipercent = round(livote/total_votes*100)
+        otooleypercent = round(otooleyvote/total_votes*100)
 
 
 
@@ -36,3 +57,4 @@ print("Election Results")
 print("-----------------")
 print(f"Total Votes:{total_votes}")
 print("------------------")
+print(f"Khan:{str(khanpercent)}% {str(khanvote)}")
